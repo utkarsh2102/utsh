a69933aa10f64df0fbcb617b0824487dd350bf56
Utkarsh Gupta <utkarsh@debian.org>
2020-02-23 00:00:26 +0530

    Add script for generating DLA entries

---
+++ lts-dla.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/zsh
+
+DLA=$1
+CVE=`cat dla | grep CVE | cut -d':' -f2`
+package=`cat dla | grep Package | cut -d':' -f2`
+version=`cat dla | grep Version | cut -d':' -f2`
+jessie=`cat dla | grep Jessie`
+
+cat << EOF >> file
+- Issued DLA $DLA-1, fixing ${CVE:1}, for ${package:1}.  
+  $jessie ${version:1}.  
+
+EOF
