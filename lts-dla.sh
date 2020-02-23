d522a61e7c683f88ffed09f776999c810eb7b97c
Utkarsh Gupta <utkarsh@debian.org>
2020-02-23 21:44:35 +0530

    Fix file name

--- lts-dla.sh
+++ lts-dla.sh
@@ -6,7 +6,7 @@ package=`cat dla | grep Package | cut -d':' -f2`
 version=`cat dla | grep Version | cut -d':' -f2`
 jessie=`cat dla | grep Jessie`
 
-cat << EOF >> file
+cat << EOF >> *.markdown
 - Issued DLA $DLA-1, fixing ${CVE:1}, for ${package:1}.  
   $jessie ${version:1}.  
 
