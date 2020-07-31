632ad0c0a6216bb0e5b35fde2a2379a97616dc19
Utkarsh Gupta <utkarsh@debian.org>
2020-08-01 03:44:20 +0530

    Add script to reserve DLA entry
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

---
+++ lts-dla.rb
@@ -0,0 +1,45 @@
+#! /usr/bin/ruby
+
+dla = ARGV[0]
+package = version = nil
+cve = []
+fix = ""
+
+file = File.read('dla')
+file.each_line do |line|
+  if line =~ /Package/
+    package = line.scan(/\w+/).last
+  end
+
+  if line =~ /Version/
+    version = line.split(': ', 2).last.chomp
+  end
+
+  cve << line.scan(/CVE-\d+-\d+/)
+end
+
+cve.flatten!
+cve.uniq!
+
+size = cve.length
+
+if size == 1
+  fix = "[#{cve[0]}](https://security-tracker.debian.org/tracker/#{cve[0]})"
+elsif size == 2
+  fix = "[#{cve[0]}](https://security-tracker.debian.org/tracker/#{cve[0]}) " \
+        "and [#{cve[1]}](https://security-tracker.debian.org/tracker/#{cve[1]})"
+elsif size > 2
+  0.upto size-2 do |i|
+    fix << "[#{cve[i]}](https://security-tracker.debian.org/tracker/#{cve[i]}), "
+  end
+  fix << "and [#{cve[size-1]}](https://security-tracker.debian.org/tracker/#{cve[size-1]})"
+else
+  puts "Some error occured. 0 elements in CVE array."
+end
+
+sentence1 = "- Issued [DLA #{dla}-1](), fixing #{fix}, for " \
+            "[#{package}](https://tracker.debian.org/pkg/#{package}).  "
+sentence2 = "  For Debian 9 Stretch, these problems have been fixed in version #{version}."
+
+puts sentence1
+puts sentence2
